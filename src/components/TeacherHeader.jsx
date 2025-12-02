df2a9c73a9123470bb232b6d7a3203dcaaa0c188
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-12-03 01:44:32 +0900

    feat: TeacherHeader 컴포넌트화

---
+++ src/components/TeacherHeader.jsx
@@ -0,0 +1,33 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { colors } from "../constants/colors";
+import QickLogo from "../assets/TeaHome/QickLogo.png";
+
+export default function TeacherHeader() {
+    return (
+        <View style={styles.title}>
+            <Image source={QickLogo} style={styles.logo}/>
+            <Text style={{fontSize: 24}}>|</Text>
+            <Text style={styles.titleText}>Teacher</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    title : {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        marginTop: 70
+    },
+    logo: {
+        width: 125,
+        height: 50
+    },
+    titleText : {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: colors.textBlack
+    },
+});
