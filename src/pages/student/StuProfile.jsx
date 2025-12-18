dbe8b1d66bda309867420f00025e2e0adbaa9f7f
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-12-18 11:09:48 +0900

    update: 페이지 함수명 수정

--- src/pages/student/StuProfile.jsx
+++ src/pages/student/StuProfile.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-export default function TeaHome() {
+export default function StuProfile() {
     return (
         <SafeAreaView style={styles.container}>
             <Text>학생 프로필</Text>
--- src/pages/teacher/TeaSignup2.jsx
+++ src/pages/teacher/TeaSignup2.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-export default function TeaHome() {
+export default function TeaSignup2() {
     return (
         <SafeAreaView style={styles.container}>
             <Text>선생님 회원가입</Text>
