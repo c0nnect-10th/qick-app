1de63ebe6ec92c0499564a2eae0678118d4444c0
parkhj2009 <eee0110010100@gmail.com>
2025-12-28 19:56:03 +0900

    feat :: 구글 로그인 관련 추가

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ROUTES } from './src/constants/routes';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from 'react-native-config';
 
 //페이지 import 
 //공통 페이지
@@ -26,34 +28,39 @@ import TeaNotification from "./src/pages/teacher/TeaNotification";
 import TeaProfile from "./src/pages/teacher/TeaProfile";
 import TeaSignup2 from "./src/pages/teacher/TeaSignup2";
 
+GoogleSignin.configure({
+  webClientId: Config.GOOGLE_WEB_CLIENT_ID,
+  offlineAccess: true,
+});
+
 function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
       {/* initialRouteName에 본인 개발하는 페이지 삽입 */}
-      <Stack.Navigator initialRouteName={ROUTES.TeaGenerateDetail} screenOptions={{ headerShown: false, contentStyle: { backgroundColor: 'white' }, }}>
+      <Stack.Navigator initialRouteName={ROUTES.Login} screenOptions={{ headerShown: false, contentStyle: { backgroundColor: 'white' }, }}>
         {/* 공통 페이지 */}
-        <Stack.Screen name={ROUTES.Login} component={Login}/>
-        <Stack.Screen name={ROUTES.Signup1} component={Signup1}/>
+        <Stack.Screen name={ROUTES.Login} component={Login} />
+        <Stack.Screen name={ROUTES.Signup1} component={Signup1} />
         {/* 학생 페이지 */}
-        <Stack.Screen name={ROUTES.StuCancel1} component={StuCancel1}/>
-        <Stack.Screen name={ROUTES.StuCancel2} component={StuCancel2}/>
-        <Stack.Screen name={ROUTES.StuDetail} component={StuDetail}/>
-        <Stack.Screen name={ROUTES.StuHome} component={StuHome}/>
-        <Stack.Screen name={ROUTES.StuProfile} component={StuProfile}/>
-        <Stack.Screen name={ROUTES.StuRanking} component={StuRanking}/>
-        <Stack.Screen name={ROUTES.StuSignup2} component={StuSignup2}/>
+        <Stack.Screen name={ROUTES.StuCancel1} component={StuCancel1} />
+        <Stack.Screen name={ROUTES.StuCancel2} component={StuCancel2} />
+        <Stack.Screen name={ROUTES.StuDetail} component={StuDetail} />
+        <Stack.Screen name={ROUTES.StuHome} component={StuHome} />
+        <Stack.Screen name={ROUTES.StuProfile} component={StuProfile} />
+        <Stack.Screen name={ROUTES.StuRanking} component={StuRanking} />
+        <Stack.Screen name={ROUTES.StuSignup2} component={StuSignup2} />
         {/* 선생 페이지 */}
-        <Stack.Screen name={ROUTES.TeaDelete} component={TeaDelete}/>
-        <Stack.Screen name={ROUTES.TeaGenerate1} component={TeaGenerate1}/>
-        <Stack.Screen name={ROUTES.TeaGenerate2} component={TeaGenerate2}/>
-        <Stack.Screen name={ROUTES.TeaGenerate3} component={TeaGenerate3}/>
-        <Stack.Screen name={ROUTES.TeaGenerateDetail} component={TeaGenerateDetail}/>
-        <Stack.Screen name={ROUTES.TeaHome} component={TeaHome}/>
-        <Stack.Screen name={ROUTES.TeaNotification} component={TeaNotification}/>
-        <Stack.Screen name={ROUTES.TeaProfile} component={TeaProfile}/>
-        <Stack.Screen name={ROUTES.TeaSignup2} component={TeaSignup2}/>
+        <Stack.Screen name={ROUTES.TeaDelete} component={TeaDelete} />
+        <Stack.Screen name={ROUTES.TeaGenerate1} component={TeaGenerate1} />
+        <Stack.Screen name={ROUTES.TeaGenerate2} component={TeaGenerate2} />
+        <Stack.Screen name={ROUTES.TeaGenerate3} component={TeaGenerate3} />
+        <Stack.Screen name={ROUTES.TeaGenerateDetail} component={TeaGenerateDetail} />
+        <Stack.Screen name={ROUTES.TeaHome} component={TeaHome} />
+        <Stack.Screen name={ROUTES.TeaNotification} component={TeaNotification} />
+        <Stack.Screen name={ROUTES.TeaProfile} component={TeaProfile} />
+        <Stack.Screen name={ROUTES.TeaSignup2} component={TeaSignup2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/pages/common/Login.jsx
+++ src/pages/common/Login.jsx
@@ -1,15 +1,176 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-export default function Login() {
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+// import auth from '@react-native-firebase/auth';
+import { ROUTES } from "../../constants/routes";
+
+export default function Login({ navigation }) {
+
+    const onGoogleLogin = async () => {
+        try {
+            console.log('구글 로그인 시작');
+
+            await GoogleSignin.hasPlayServices({
+                showPlayServicesUpdateDialog: true,
+            });
+
+            // 캐시 제거
+            // await GoogleSignin.signOut();
+            // await GoogleSignin.revokeAccess();
+
+            const res = await GoogleSignin.signIn();
+
+            console.log('FULL RESPONSE:', res);
+            console.log('idToken:', res.idToken);
+
+            navigation.replace(ROUTES.Signup1);
+
+        } catch (error) {
+            console.log('구글 로그인 실패:', error);
+        }
+    };
+
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text>공통 로그인</Text>
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require("../../assets/Auth/Qick.png")} />
+                <Text style={styles.title}>계정 로그인</Text>
+            </View>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.primary]}>
+                    <View style={styles.row}>
+                        <Image
+                            source={require("../../assets/Auth/Bind.png")}
+                            style={styles.icon}
+                        />
+                        <Text style={styles.primaryText}>도담도담으로 계속하기</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button} onPress={onGoogleLogin}>
+                    <View style={styles.row}>
+                        <Image
+                            source={require("../../assets/Auth/Google.png")}
+                            style={styles.icon}
+                        />
+                        <Text style={styles.secondaryText}>Google로 계속하기</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.bottom}>
+                <TouchableOpacity style={styles.teacherButton}>
+                    <Text style={styles.secondaryText}>Teacher로 계속하기</Text>
+                </TouchableOpacity>
+
+                <View style={styles.divider} />
+
+                <Text style={styles.policy}>
+                    The Google
+                    <Text style={styles.colored_policy}> Privacy Policy</Text>
+                    {" and "}
+                    <Text style={styles.colored_policy}>Terms of Service</Text>
+                </Text>
+            </View>
+
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, 
+        flex: 1
+    },
+    logoContainer: {
+        alignItems: "center",
+        marginTop: 181
+    },
+
+    logo: {
+        width: 128,
+        height: 72,
+        resizeMode: "contain",
+        marginBottom: 16,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "700",
+        color: "#1F1F1F"
+    },
+    buttonsContainer: {
+        paddingHorizontal: 24,
+        marginTop: 90,
+        gap: 8
+    },
+    primary: {
+        backgroundColor: "rgba(2,131,241,0.2)",
+    },
+
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 8,
+    },
+
+    icon: {
+        width: 20,
+        height: 20,
+    },
+
+    primaryText: {
+        fontSize: 14,
+        fontWeight: "600",
+        color: "#FFFFFF",
+    },
+
+    secondaryText: {
+        fontSize: 14,
+        fontWeight: "600",
+        color: "#474747",
+    },
+
+    bottom: {
+        marginTop: "auto",
+        alignItems: "center",
+        paddingBottom: 24,
+    },
+
+    teacherButton: {
+        width: "100%",
+        height: 40,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: "#EBEBEB",
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 16,
+    },
+
+    divider: {
+        width: "100%",
+        height: 1,
+        backgroundColor: "#EBEBEB",
+        marginBottom: 12,
+    },
+
+    policy: {
+        fontSize: 12,
+        color: "#858585",
+        textAlign: "center",
+    },
+    colored_policy: {
+        fontSize: 12,
+        color: "#FF6000",
+        textAlign: "center",
+    },
+    button: {
+        height: 48,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: "#EBEBEB",
+        justifyContent: "center",
+        alignItems: "center",
     },
 });
