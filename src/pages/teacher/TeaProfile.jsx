582aa3b616d9c379c9ad41c1818895fb590b85bf
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-01-08 23:24:59 +0900

    feat: 스와이프해서 화면 이동 구현

--- src/pages/teacher/TeaProfile.jsx
+++ src/pages/teacher/TeaProfile.jsx
@@ -4,15 +4,9 @@ import TeacherHeader from "../../components/TeacherHeader";
 import { colors } from "../../constants/colors";
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
-export default function TeaProfile({ navigation }) {
+export default function TeaProfile() {
     return (
         <SafeAreaView style={styles.container}>
-            <TeacherHeader />
-            <View style={styles.nav}>
-                <TouchableOpacity style={styles.navButt} onPress={() => navigation.navigate('TeaHome')}><Text style={styles.navButtText}>홈</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.navButt} onPress={() => navigation.navigate('TeaNotification')}><Text style={styles.navButtText}>알림</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.navButt}><Text style={[styles.navButtText, styles.now]}>프로필</Text></TouchableOpacity>
-            </View>
             <View style={styles.profile}>
                 <View style={styles.profileImg}></View>
                 <Text style={{fontWeight: 600, fontSize: 20, marginLeft: 15}}>성함</Text>
@@ -46,44 +40,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'white'
     },
-    nav : {
-        flexDirection: 'row',
-        backgroundColor: colors.gray100,
-        width: '90%',
-        height: 30,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingLeft: 3,
-        paddingRight: 3,
-        borderRadius: 5,
-        marginTop: 20
-    },
-    now : {
-        backgroundColor: 'white',
-        borderRadius: 5,
-        height: 25,
-        elevation: 3
-    },
-    navButt : {
-        width: '32%',
-        borderRadius: 5,
-        height: 25,
-        justifyContent: 'center'
-    },
-    navButtText : {
-        textAlign: 'center',
-        fontWeight: '500',
-        verticalAlign: 'middle'
-    },
     profile: {
         height: '20%',
         width: '100%',
         flexDirection: 'row',
         alignItems: 'center',
-        gap: 5
+        gap: 5,
+        marginBottom: 20
     },
     profileImg: {
-        height: '60%',
+        height: '75%',
         borderWidth: 1,
         aspectRatio: 1,
         borderRadius: 80,
